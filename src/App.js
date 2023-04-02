5d1dec64a30f2b305a1e231f055fde2b0aefbda8
moinul <moinul@moinuls-MacBook-Pro.local>
2023-04-02 09:04:38 +0600

    topbar add with color theme

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import './App.css';
+import { useState } from "react";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider } from "@mui/material";
-
+import Topbar from './pages/global/Topbar';
 function App() {
   const [theme, colorMode] = useMode();
   const [isSidebar, setIsSidebar] = useState(true);
@@ -11,6 +10,8 @@ function App() {
         <CssBaseline />
         <div className="app">
         <main className="content">
+        <Topbar setIsSidebar={setIsSidebar} />
+
         </main>
         </div>
     </ThemeProvider>   
