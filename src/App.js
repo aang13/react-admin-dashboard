8282621631a2e1cb77e547fe0aa4ac7ebcf2b6c5
moinul <moinul@moinuls-MacBook-Pro.local>
2023-04-02 10:09:23 +0600

    team added

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import { Routes, Route } from "react-router-dom";
 
 // import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard";
-// import Team from "./pages/team";
+import Team from "./pages/team";
 // import Invoices from "./pages/invoices";
 // import Contacts from "./pages/contacts";
 // import Bar from "./pages/bar";
@@ -31,9 +31,9 @@ function App() {
           <main className="content">
             <Topbar setIsSidebar={setIsSidebar} />
             <Routes>
-              { <Route path="/" element={<Dashboard />} />
-              /*<Route path="/team" element={<Team />} />
-              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              {/* <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/bar" element={<Bar />} />
@@ -42,6 +42,7 @@ function App() {
               <Route path="/faq" element={<FAQ />} />
               <Route path="/calendar" element={<Calendar />} />
               <Route path="/geography" element={<Geography />} /> */}
+
             </Routes>
           </main>
         </div>
