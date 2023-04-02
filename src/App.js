e8f4f60eed36deac89f4c8f3bc9fc8341bb4ee55
moinul <moinul@moinuls-MacBook-Pro.local>
2023-04-02 11:04:53 +0600

    routes and pages added for all charts

--- src/App.js
+++ src/App.js
@@ -5,17 +5,16 @@ import Topbar from './pages/global/Topbar';
 import Sidebar from "./pages/global/Sidebar";
 import { Routes, Route } from "react-router-dom";
 
-// import Sidebar from "./pages/global/Sidebar";
 import Dashboard from "./pages/dashboard";
 import Team from "./pages/team";
 import Invoices from "./pages/invoices";
 import Contacts from "./pages/contacts";
-// import Bar from "./pages/bar";
+import Bar from "./pages/bar";
 import Form from "./pages/form";
-// import Line from "./pages/line";
-// import Pie from "./pages/pie";
-// import FAQ from "./pages/faq";
-// import Geography from "./pages/geography";
+import Line from "./pages/line";
+import Pie from "./pages/pie";
+import FAQ from "./pages/faq";
+import Geography from "./pages/geography";
 import Calendar from "./pages/calendar/calendar";
 
 function App() {
@@ -37,11 +36,11 @@ function App() {
               <Route path="/invoices" element={<Invoices />} />
               <Route path="/form" element={<Form />} />
               <Route path="/calendar" element={<Calendar />} />
-              {/* <Route path="/bar" element={<Bar />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/faq" element={<FAQ />} />
               <Route path="/pie" element={<Pie />} />
               <Route path="/line" element={<Line />} />
-              <Route path="/faq" element={<FAQ />} />
-              <Route path="/geography" element={<Geography />} /> */}
+              <Route path="/geography" element={<Geography />} />
 
             </Routes>
           </main>
